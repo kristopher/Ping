a5b7bb0dd9a350836867a9939b6cbd065dba3ee3
Kristopher Chambers <kristopher.chambers@gmail.com>
2009-07-02 21:53:29 -0400

    Adding centimeters conversion.

--- Ping.cpp
+++ Ping.cpp
@@ -1,12 +1,18 @@
 #include "WConstants.h"
 #include "Ping.h"
 
-Ping::Ping(int sig_pin) {
+Ping::Ping(int sig_pin, int units) {
   _sig_pin = sig_pin;
+  _units = units;
+  if (units == INCHES) {
+    conversion_function = &Ping::microsecondsToInches;
+  } else {
+    conversion_function = &Ping::microsecondsToCentimeters;    
+  }
 }
 
 long Ping::read(int timeout) {
-  long duration, inches;
+  long duration;
 
   // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
   // We give a short LOW pulse beforehand to ensure a clean HIGH pulse.
@@ -21,12 +27,10 @@ long Ping::read(int timeout) {
   // pulse whose duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
   pinMode(_sig_pin, INPUT);
-  duration = pulseIn(_sig_pin, HIGH, timeout);
-
+  duration = pulseIn(_sig_pin, HIGH, timeout); // Returns 0 if timeout is reached
+  
   // convert the time into a distance
-  inches = microsecondsToInches(duration);
- 
-  return inches;
+  return (this->*conversion_function)(duration);
 }
 
 long Ping::microsecondsToInches(long microseconds) {
@@ -36,4 +40,11 @@ long Ping::microsecondsToInches(long microseconds) {
   // and return, so we divide by 2 to get the distance of the obstacle.
   // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
   return microseconds / 74 / 2;
+}
+
+long Ping::microsecondsToCentimeters(long microseconds) {
+  // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+  // The ping travels out and back, so to find the distance of the
+  // object we take half of the distance travelled.
+  return microseconds / 29 / 2;
 }
--- Ping.h
+++ Ping.h
@@ -1,13 +1,17 @@
 #ifndef Ping_h
-
 #define Ping_h
+#define INCHES 1
+#define CENTIMETERS 2
 
 class Ping {
   private:
     long microsecondsToInches(long);
+    long microsecondsToCentimeters(long);
     int _sig_pin;
+    int _units;
+    long (Ping::*conversion_function)(long);
   public:
-    Ping(int);
+    Ping(int sig_pin, int units = INCHES);
     long read(int timeout = 20000);
 };
 
